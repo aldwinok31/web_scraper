1127cf4b7130830ea2a5f030016eadc97a6080be
Aldwin Tablanten <atablante@auksilyo.com>
2022-03-10 06:33:29 +0800

    Fetch high point market

---
+++ activities.py
@@ -0,0 +1,186 @@
+from selenium import webdriver
+from functions import *
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+import json
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import time
+
+import xlsxwriter
+
+## LAS VEGAS MARKET
+class LVMActivity:
+    def __init__(self,url):
+        self.url = url
+        self.array_current = 0
+        self.array_href = []
+        self.array_full_data = []
+        pass
+
+    def start_driver(self):
+        self.driver = webdriver.Chrome('./chromedriver.exe')
+        self.driver.maximize_window()
+        self.driver.get(self.url)
+        self.start()
+        pass
+
+    def start(self):
+        searchElement(self.driver,self.callback_filteredArray)
+
+    def callback_filteredArray(self,array):
+        self.array_of_elements = array
+        self.array_max = len(array)
+        self.scroll_to(array[self.array_current])
+        array[self.array_current].click()
+        getHrefs(self.driver,self.get_a,self)
+
+    def back(self):
+        self.driver.execute_script("window.history.go(-1)")
+
+    def scroll_to(self,el):
+        self.driver.execute_script("arguments[0].scrollIntoView();",el)
+
+    def get_a(self,_array):
+        self.array_current = self.array_current + 1
+
+        if(self.array_current == self.array_max):
+            self.array_href += _array
+            self.write_to_excel_hrefs()
+            print("HREFS wrote")
+            view_link(self.array_href,self,self.driver)
+            self.write_to_excel_datas()
+            print("Whole Data wrote")
+        else:
+            self.array_href += _array
+            self.back()
+            WebDriverWait(self.driver,20).until(EC.presence_of_element_located((By.CLASS_NAME,"imc-campus-view--floor-name")))
+            self.start()
+
+
+    ## WRITE TO FULL DATA
+    def write_to_excel_datas(self):
+        workbook = xlsxwriter.Workbook("competitors_data_filtered.xlsx")
+        worksheet = workbook.add_worksheet("Comp")
+        row = 0
+        col = 0
+
+        worksheet.write(row,col,"Name")
+        worksheet.write(row,col + 1,"Market Link")
+        worksheet.write(row,col + 2,"Mattress / Adjustable")
+        worksheet.write(row,col + 3,"Website URL")
+        worksheet.write(row,col + 4,"Contact")
+
+        worksheet.write(row,col + 5,"Product Notes")
+
+        row += 1
+        for i in self.array_full_data:
+            worksheet.write(row,col,i["Name"])
+            worksheet.write(row,col + 1, i["MarketLink"])
+            worksheet.write(row,col + 2, i["categories"])
+            worksheet.write(row,col + 3, i["website"])
+            worksheet.write(row,col + 4,i["contact"])
+
+            worksheet.write(row,col + 5, i["body"])
+            row += 1
+
+        workbook.close()
+
+
+
+
+    def write_to_excel_hrefs(self):
+        workbook = xlsxwriter.Workbook("possible_competitors_href.xlsx")
+        worksheet = workbook.add_worksheet("Comp")
+        row = 0
+        col = 0
+
+        worksheet.write(row,col,"Name")
+        worksheet.write(row,col + 1,"Link")
+
+        row +=1
+
+        for i in self.array_href:
+            worksheet.write(row,col,i['text'])
+            worksheet.write(row,col+1,i['link'])
+            row += 1
+
+        workbook.close()
+
+## HIGH POINT MARKET
+class HPMActivity():
+    def __init__(self,url):
+        self.url = url
+
+    def start_driver(self):
+        self.driver = webdriver.Chrome('./chromedriver.exe')
+        self.driver.maximize_window()
+        self.driver.get(self.url)
+        self.start()
+
+
+    def start(self):
+        get_HPM_links(self.driver,self,self.filter_HPM_links)
+
+    def filter_HPM_links(self,_arr):
+        self.array_href = _arr
+        self.write_to_excel_hrefs()
+        filtered_arr = filter_HPM(self.driver,self,_arr)
+        self.array_full_data = filtered_arr
+        self.write_to_excel_datas()
+
+
+    def back(self):
+        self.driver.execute_script("window.history.go(-1)")
+
+    def scroll_to(self,el):
+        self.driver.execute_script("arguments[0].scrollIntoView();",el)
+
+    def write_to_excel_hrefs(self):
+        workbook = xlsxwriter.Workbook("possible_competitors_href_HVM.xlsx")
+        worksheet = workbook.add_worksheet("Comp")
+        row = 0
+        col = 0
+
+        worksheet.write(row,col,"Name")
+        worksheet.write(row,col + 1,"Link")
+        worksheet.write(row,col + 2,"Categories")
+
+
+        row +=1
+
+        for i in self.array_href:
+            worksheet.write(row,col,i['text'])
+            worksheet.write(row,col+1,i['link'])
+            worksheet.write(row,col+2,i['categories'])
+
+            row += 1
+
+        workbook.close()
+
+    def write_to_excel_datas(self):
+        workbook = xlsxwriter.Workbook("competitors_data_filtered_HVM.xlsx")
+        worksheet = workbook.add_worksheet("Comp")
+        row = 0
+        col = 0
+
+        worksheet.write(row,col,"Name")
+        worksheet.write(row,col + 1,"Market Link")
+        worksheet.write(row,col + 2,"Mattress / Adjustable")
+        worksheet.write(row,col + 3,"Website URL")
+        worksheet.write(row,col + 4,"Contact")
+
+        worksheet.write(row,col + 5,"Product Notes")
+
+        row += 1
+        for i in self.array_full_data:
+            worksheet.write(row,col,i["Name"])
+            worksheet.write(row,col + 1, i["MarketLink"])
+            worksheet.write(row,col + 2, i["categories"])
+            worksheet.write(row,col + 3, i["website"])
+            worksheet.write(row,col + 4,i["contact"])
+
+            worksheet.write(row,col + 5, i["body"])
+            row += 1
+
+        workbook.close()
--- functions.py
+++ functions.py
@@ -2,6 +2,10 @@ from selenium.webdriver.common.by import By
 from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from pprint import pprint
+import time
+
+#### FOR LAS VEGAS MARKET
 def searchElement(driver,callback):
     WebDriverWait(driver,3).until(EC.presence_of_element_located((By.CLASS_NAME,"imc-campus-view--floor-name")))
 
@@ -127,3 +131,135 @@ def view_link(array_href,context,driver):
 
         except TimeoutException:
             print("Error viewing page")
+
+###### FOR HIGH POINT MARKET
+def get_HPM_links(driver,context,callback):
+    href_data = []
+    href_data += set_adjustable(driver,context)
+    context.back()
+    href_data += set_mattresses(driver,context,href_data)
+    callback(href_data)
+
+    #cat4[0].click()
+
+def set_mattresses(driver,context,adj_data):
+    WebDriverWait(driver,20).until(EC.presence_of_element_located((By.CLASS_NAME,"cd-dropdown-trigger")))
+
+    href_data = []
+
+    filter = driver.find_elements(By.CLASS_NAME,"cd-dropdown-trigger")
+    search = driver.find_elements(By.CLASS_NAME,"search")
+
+    filter[0].click()
+    categories = driver.find_elements(By.CLASS_NAME,"has-children.outer")
+    #context.scroll_to(categories[0])
+
+    time.sleep(2)
+
+    categories[0].click()
+
+
+    time.sleep(2)
+    cat1 = categories[0].find_elements(By.XPATH,"./ul/li")
+    body = categories[0].find_elements(By.XPATH,"./ul")
+    driver.execute_script("arguments[0].scrollTop = arguments[0].scrollHeight", body[0])
+    time.sleep(5)
+    cat1[14].click()
+  # TODO:
+    time.sleep(10)
+    WebDriverWait(driver,20).until(EC.presence_of_element_located((By.CLASS_NAME,"exh-name")))
+    WebDriverWait(driver,20).until(EC.element_to_be_clickable((By.CLASS_NAME,"exh-name")))
+
+    exhibitor = driver.find_elements(By.CLASS_NAME,"exh-name")
+    for item in exhibitor:
+        a = item.find_elements(By.XPATH,"./../..")
+        add = 1
+        for i in adj_data:
+            if(i['text'] == item.get_attribute("innerText")):
+                i["categories"] += "/ Mattresses"
+                add = 0
+                break
+        if(add):
+            data = {'text':item.get_attribute("innerText"),'link':a[0].get_attribute("href"),'categories':"Mattresses"}
+            href_data.append(data)
+
+
+    return href_data
+
+
+def set_adjustable(driver,context):
+    WebDriverWait(driver,20).until(EC.presence_of_element_located((By.CLASS_NAME,"cd-dropdown-trigger")))
+
+    href_data = []
+
+    filter = driver.find_elements(By.CLASS_NAME,"cd-dropdown-trigger")
+    context.scroll_to(filter[0])
+    filter[0].click()
+
+    categories = driver.find_elements(By.CLASS_NAME,"has-children.outer")
+    time.sleep(1)
+
+    categories[0].click()
+
+    categories2 = categories[0].find_elements(By.CLASS_NAME,"has-children")
+    time.sleep(1)
+    categories2[3].click()
+
+    categories3 = categories2[3].find_elements(By.XPATH,"//ul[@class='innerlist']/li")
+
+    cat4 = categories3[2].find_elements(By.CLASS_NAME,"filterLink.c")
+    WebDriverWait(driver,10).until(EC.element_to_be_clickable(cat4[0]))
+    #WebDriverWait(driver,10).until(EC.presence_of_element_located(cat4[0]))
+    time.sleep(5)
+    cat4[0].click()
+
+    time.sleep(10)
+    WebDriverWait(driver,20).until(EC.presence_of_element_located((By.CLASS_NAME,"exh-name")))
+    WebDriverWait(driver,20).until(EC.element_to_be_clickable((By.CLASS_NAME,"exh-name")))
+
+    exhibitor = driver.find_elements(By.CLASS_NAME,"exh-name")
+    for item in exhibitor:
+        context.scroll_to(item)
+        a = item.find_elements(By.XPATH,"./../..")
+        data = {'text':item.get_attribute("innerText"),'link':a[0].get_attribute("href"),'categories':"Adjustable Bed"}
+        href_data.append(data)
+
+    return href_data
+
+def filter_HPM(driver,context,_arr):
+    newarr = []
+    for i in _arr:
+        driver.get(i['link'])
+        WebDriverWait(driver,20).until(EC.presence_of_element_located((By.CLASS_NAME,"exh-info")))
+        time.sleep(1)
+
+        con1 = driver.find_element_by_id("ctl00_ContentPlaceHolder1_lblLocation")
+        con2 = driver.find_element_by_id("ctl00_ContentPlaceHolder1_lblBusStop")
+        con3 = driver.find_element_by_id("ctl00_ContentPlaceHolder1_lblNeighborhood")
+        con4 = driver.find_element_by_id("ctl00_ContentPlaceHolder1_lblCorpPhone")
+        web = driver.find_element_by_id("ctl00_ContentPlaceHolder1_lnkWebsite")
+
+        contact =""
+        contact += con1.get_attribute("innerText")
+        contact += "\n"
+        contact += con2.get_attribute("innerText")
+        contact += "\n"
+        contact += con3.get_attribute("innerText")
+        contact += "\n"
+        contact += con4.get_attribute("innerText")
+
+        weblink = web.get_attribute("href")
+        categories = i['categories']
+        about = ""
+        try:
+            WebDriverWait(driver,20).until(EC.presence_of_element_located((By.ID,"ctl00_ContentPlaceHolder1_lblDescription")))
+            about = driver.find_element_by_id("ctl00_ContentPlaceHolder1_lblDescription").get_attribute("innerText")
+        except TimeoutException:
+            about = "NONE"
+
+        new_data = {"Name":i['text'],"MarketLink":i["link"],"body":about,"categories":categories,"website":weblink,"contact":contact}
+        newarr.append(new_data)
+        context.back()
+        time.sleep(2)
+
+    return newarr
